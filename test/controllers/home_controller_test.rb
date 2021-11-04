e4282e57fb1815b7e4fe5528328352df82876a17
alexcodeable <alexokomado@gmail.com>
2021-11-04 15:15:07 +0100

    initial commit
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get home_index_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get home_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get home_contact_url
+    assert_response :success
+  end
+
+  test "should get order" do
+    get home_order_url
+    assert_response :success
+  end
+
+  test "should get catering" do
+    get home_catering_url
+    assert_response :success
+  end
+end
